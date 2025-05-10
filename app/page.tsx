cf4f877dc93b0a695993397a71ce074f642a4f48
jberkowitz89 <joey.berkowitz@vercel.com>
2025-05-09 22:22:07 -0500

    lets see if page fixes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,15 @@ export default function Home() {
     <main className="min-h-screen bg-gradient-to-b from-green-50 to-white">
       <Header />
       <div className="container mx-auto px-4 py-8">
-        <ClientCalendarWrapper />
+        {/* Add the export button above the calendar */}
+        <div className="flex justify-end mb-4">
+          <PdfExportButton />
+        </div>
+
+        {/* Make sure the calendar wrapper has the id that the PDF export function looks for */}
+        <div id="calendar-container">
+          <ClientCalendarWrapper />
+        </div>
       </div>
     </main>
   )
